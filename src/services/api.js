195713f312755a8e51127324fe85db3a4525d767
Gianluca Carra <contact@giancarra.com>
2024-06-21 16:45:14 -0400

    API adjust

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://food-explorer-loqy.onrender.com",
+  baseURL: "https://food-explorer-loqy.onrender.com", 
   withCredentials: true
 });
